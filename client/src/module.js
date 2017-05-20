4d1b12862244bdfd48ef7d9a00c97b8b092aed93
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2017-05-20 19:57:53 -0400

    Page Animation Styling

--- client/src/module.js
+++ client/src/module.js
@@ -1,8 +1,9 @@
 import angular from "angular";
 import router from "angular-route";
 import resource from "angular-resource";
+import animate from "angular-animate";
 import { Module } from "ng-harmony-module";
 
-var module = new Module("compucorp", ["ngRoute", "ngResource"]);
+var module = new Module("compucorp", ["ngRoute", "ngResource", "ngAnimate"]);
 
 export default module;

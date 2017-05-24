dfba71f406b3278da3b8a3ec9d8df5a58d6b9e3e
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2017-05-24 16:20:23 -0400

    intermediate commit for co-debug purposes

--- client/src/services/spotify.js
+++ client/src/services/spotify.js
@@ -28,46 +28,48 @@ export class SpotifyService extends Srvc {
 			adapter: "idb",           // <- storage-adapter
 			multiInstance: false         // <- multiInstance (default: true)
 		});
-		this.artists = await this.db.collection({
-			name: "artist",
+		await this.db.collection({
+			name: "artists",
 			schema: ArtistSchema
 		});
-		this.albums = await this.db.collection({
-			name: "album",
+		await this.db.collection({
+			name: "albums",
 			schema: AlbumSchema
 		});
 		this.initialized.resolve();
 	}
 	async _search (q) {
-		let localAlbums = await this.localAlbumSearch(q);
-		let localArtists = await this.localArtistSearch(q);
-		let offset = localArtists > localAlbums ? localArtists.length.toString() : localAlbums.length.toString();
-		//this.log(`Offset is ${offset} ...`);
-		return this.$http.get(`https://api.spotify.com/v1/search?q="${q}"&type=album,artist&offset=${offset}`);
+		return this.$http.get(`https://api.spotify.com/v1/search?q="${q}"&type=album,artist`);
 	}
 	localAlbumSearch (q) {
-		return this.albums.find()
-			.where({ "name": new RegExp(`.*${q}.*`, "i") })
+		this.db.albums._queryCache.destroy();
+		return this.db.albums
+			.find()
+			.where("name")
+			.regex(new RegExp(q, "i"))
 			.exec();
 	}
 	localArtistSearch (q) {
-		return this.artists.find()
-			.where({ "name": new RegExp(`.*${q}.*`, "i") })
+		this.db.artists._queryCache.destroy();
+		return this.db.artists
+			.find()
+			.where("name")
+			.regex(new RegExp(q, "i"))
 			.exec();
 	}
 	async search (q) {
 		let results = await this._search(q);
 		results.data.albums.items.forEach((doc) => {
-			this.albums.upsert(doc);
+			this.db.albums.upsert(doc);
 		});
 		results.data.artists.items.forEach((doc) => {
-			this.artists.upsert(doc);
+			this.db.artists.upsert(doc);
 		});
 	}
 	subscribeArtists (observer) {
-		this.artists.$.subscribe(observer);
+		this.db.artists.$.subscribe(observer);
 	}
 	subscribeAlbums (observer) {
-		this.albums.$.subscribe(observer);
+		this.db.albums.$.subscribe(observer);
 	}
 }

34dade60ea24719676785c6c5d2f6ba867c6d6a6
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2017-05-29 16:03:28 -0400

    a clean plate for building again

--- src/pages/landing.js
+++
@@ -1,8 +0,0 @@
-import { Controller as Ctrl } from "ng-harmony-core";
-import { Controller } from "ng-harmony-decorator";
-
-@Controller({
-	module: "compucorp",
-	name: "LandingPageCtrl"
-})
-export class LandingPageCtrl extends Ctrl {};

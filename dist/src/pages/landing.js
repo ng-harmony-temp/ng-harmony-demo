08f763f99d0b2b98771e724a04ac9b3bc8759fdb
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2017-05-29 16:07:32 -0400

    release-candidate 1.0.1

---
+++ dist/src/pages/landing.js
@@ -0,0 +1,8 @@
+import { Controller as Ctrl } from "ng-harmony-core";
+import { Controller } from "ng-harmony-decorator";
+
+@Controller({
+	module: "compucorp",
+	name: "LandingPageCtrl"
+})
+export class LandingPageCtrl extends Ctrl {};
